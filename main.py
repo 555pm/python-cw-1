da17f2d4379cedfcdb7524cf7ea1389e533446ea
Sae <142683788+555pm@users.noreply.github.com>
2023-08-23 19:46:25 +0300

    classwork

--- main.py
+++ main.py
@@ -14,3 +14,20 @@
 
 
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+
+print('Sarah')
+numb1 = 7
+numb2 = 1
+print(numb1 + numb2)
+print(10/5)
+print(6*5)
+
+length = 7
+width = 10
+area=(7*10)
+print(F'the result is {area}')
+
+length1= 5
+width1= 3
+area1= 15
+print(f'If the length of the rectangle is {length1},and width is {width1},then the area is {area1}')
